4f84912de0bb961b3a8ad30f0cabac0802695bd5
Yang <zhy0216@gmail.com>
2013-04-27 10:32:09 +0800

    make the floor for tweet

--- ezlog2/model/tweet.py
+++ ezlog2/model/tweet.py
@@ -12,6 +12,7 @@ class Tweet(db.Document):
     content            = db.StringField(required = True)
     type               = db.StringField(default = "text")
     originalid         = db.StringField(default = "")
+    retweetid          = db.StringField(default = "")
     retweet_comment    = db.StringField()
     create_date        = db.DateTimeField(default = dt.now)
 
@@ -21,7 +22,7 @@ class Tweet(db.Document):
         'allow_inheritance': False,
         'index_types': False,
     }
-    
+
     def __ini__(self):
         self._comments = None
 
@@ -31,8 +32,9 @@ class Tweet(db.Document):
     @classmethod
     def retweetit(cls, originalid, comment, poster):
         t   = cls.get_tweet_byid(originalid)
-        ret = cls(content         = t.content, 
-                originalid      = originalid, 
+        ret = cls(content       = t.content,
+                originalid      = t.originalid or originalid,
+                retweetid       = originalid,
                 retweet_comment = comment,
                 poster          = poster).save()
         return ret
@@ -44,13 +46,17 @@ class Tweet(db.Document):
     def original_tweet(self):
         return Tweet.objects(id=self.originalid).first()
 
+    @property
+    def retweet_tweet(self):
+        return Tweet.objects(id=self.retweetid).first()
+
     #TO-DO: it should be follower
     @classmethod
     def get_tweets_foruser(cls,user, limit = 20, offset = 0):
         start       = offset*limit
         end         = offset*limit+limit
         following_users = user.get_following_users()
-        
+
         return cls.objects(poster__in=[x.id for x in following_users] + [user.id])\
                   .order_by("-create_date")[start:end]
 
@@ -69,12 +75,10 @@ class Tweet(db.Document):
     def comment_on(self, commenter, content):
         pass
 
-    def retweet(self):
-        pass
-        
+
     @property
     def retweet_counter(self):
-        return len(Tweet.objects(originalid=str(self.id)))
+        return len(Tweet.objects(retweetid=str(self.id)))
     @property
     def comment_counter(self):
         return len(Comment.objects(tweet=self.id))
@@ -82,7 +86,7 @@ class Tweet(db.Document):
     def comments(self):
         self._comments = Comment.get_comments_bytweetid(self.id)
         return self._comments
-        
+
 
 
 
