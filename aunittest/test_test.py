3ede71ca9bbcfa06644a82ec41954905fb2ce0e4
Yang <zhy0216@gmail.com>
2013-05-22 13:31:57 +0800

    add unittets

--- aunittest/test_test.py
+++ aunittest/test_test.py
@@ -1,8 +1,5 @@
 # coding: utf8
 
-import sys
-
-err = sys.stderr
 
 def test_nose_is_working():
     assert 1+1>=2
---
+++ aunittest/test_user.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+from nose.tools import nottest, istest, raises,eq_,ok_
+
+from ezlog2.model import User
+from ezlog2.util import sha224
+
+class TestUser(unittest.TestCase):
+    def setUp(self):
+        self.user   = User(email="fake",nickname="12",password=sha224("fakeone")).save()
+        
+    def test_auth(self):
+        ok_(User.is_valid("fake",sha224("fakeone")))
+        user = User.validate_user("fake",sha224("fakeone"))
+        eq_(user.nickname,self.user.nickname)
+        
+    def test_get_user_by_id(self):
+        user = User.get_user_by_id(self.user.id)
+        eq_(user,self.user)
+        
+    def test_email_exist(self):
+        pass
+        
+    def test_nickname_exist(self):
+        pass
+
+    def tearDown(self):
+        User.drop_collection()
