8776c30c2523addfc636686f96465b533d5c0391
Yang <zhy0216@gmail.com>
2013-05-22 22:19:06 +0800

    add logout view

--- ezlog2/blueprints/admin/view.py
+++ ezlog2/blueprints/admin/view.py
@@ -1,5 +1,5 @@
 ﻿# coding:utf-8
-from flask import Blueprint, g,session,redirect,url_for
+from flask import Blueprint, g,session,redirect,url_for,flash
 from flask.ext.admin import Admin, BaseView,AdminIndexView, expose
 from flask.ext.admin.contrib.mongoengine import ModelView
 
@@ -19,6 +19,7 @@ class MyAdminView(AdminIndexView,AuthBase):
     def logout(self):
         if author_admin():
             session.pop("admin")
+            flash(u"管理员登出成功")
         return redirect(url_for('main'))
         
     def is_accessible(self):
