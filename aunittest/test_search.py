a553fbd867b6d33bece8d18468022a757ee378b7
Yang <zhy0216@gmail.com>
2013-06-01 20:10:20 +0800

    add necessary unittest template

---
+++ aunittest/test_search.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+from nose.tools import nottest, istest, raises,eq_,ok_
+
+
+class TestSearch(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
