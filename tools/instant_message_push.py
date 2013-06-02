548988a8873b8f8ebdbcbfb9810abfedb136225b
Yang <zhy0216@gmail.com>
2013-06-02 23:19:55 +0800

    use gevent method

--- tools/instant_message_push.py
+++ tools/instant_message_push.py
@@ -4,18 +4,19 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")
 
 from redis import Redis
 from flask import request,Response, stream_with_context,session
-
+import gevent
 
 from ezlog2 import app
 
+redis = Redis()
 
 @app.route("/notify_user/<userid>")
 def stream(userid):
-    redis = Redis()
     def generate():
         pubsub = redis.pubsub()
         pubsub.subscribe("notify-%s"%userid)
         for event in pubsub.listen():
+            gevent.sleep(0.5)
             if event["type"] == "message":
                 yield "data: %s\r\n\r\n" % event["data"]
     return Response(stream_with_context(generate()),
@@ -23,8 +24,11 @@ def stream(userid):
                     mimetype="text/event-stream")
 
 if __name__ == '__main__':
-    # from gevent import monkey; monkey.patch_all()
-    app.debug = True
-    app.run(port=5005,threaded=True)
-
+    from gevent.pywsgi import WSGIServer
+    from gevent import monkey; monkey.patch_all()
+    
+    # app.debug = True
+    # app.run(port=5005,threaded=True)
+    http_server = WSGIServer(('127.0.0.1', 5005), app)
+    http_server.serve_forever()
 
